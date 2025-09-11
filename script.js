a14e5ee807b5a3017ccdd81c108ab984c5c18590
Ruaidhri O'Neill <49767184+ruaidhri13@users.noreply.github.com>
2025-09-11 23:31:43 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scroll behavior (if not using CSS scroll-behavior)
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
-});
